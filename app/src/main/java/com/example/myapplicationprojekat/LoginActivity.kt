fc53f46ad70b66c2a17061abed8d1c96ad5658d0
Lana Matic <lanamatic000@gmail.com>
2024-04-13 13:50:27 +0200

    login class

---
+++ app/src/main/java/com/example/myapplicationprojekat/LoginActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplicationprojekat
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var edtUsername: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignup: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_fragment)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        edtUsername = findViewById(R.id.username)
+        edtPassword = findViewById(R.id.password)
+        btnLogin = findViewById(R.id.login)
+        btnSignup = findViewById(R.id.signup)
+
+//        btnSignup.setOnClickListener {
+//            val intent = Intent(this, SignUp::class.java)
+//            startActivity(intent)
+//        }
+
+    }
+}
