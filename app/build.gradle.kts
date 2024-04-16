92311beffc3dc38090038487b039fc05318ca86b
stasadjordjevic <stasadjordjevic2@gmail.com>
2024-04-16 16:51:43 +0200

    cloud firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/myapplicationprojekat/SigupActavity.kt
+++ app/src/main/java/com/example/myapplicationprojekat/SigupActavity.kt
@@ -12,21 +12,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.Objects
 
 class SigupActavity : AppCompatActivity() {
 
 
     private lateinit var edtUsername: EditText
-    private lateinit var editEmail: EditText
+    private lateinit var edtEmail: EditText
     private lateinit var edtPassword: EditText
     private lateinit var btnSignup: Button
 
     private lateinit var auth: FirebaseAuth
+//    val db = Firebase.firestore
+
+    private lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        //initialize firebase auth
         auth = FirebaseAuth.getInstance()
+        //initialize firebase firestore
+        db = FirebaseFirestore.getInstance()
 
         setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup_fragment)) { v, insets ->
@@ -36,11 +46,11 @@ class SigupActavity : AppCompatActivity() {
         }
         btnSignup = findViewById(R.id.button2)
         edtUsername = findViewById(R.id.username)
-        editEmail = findViewById(R.id.email)
+        edtEmail = findViewById(R.id.email)
         edtPassword = findViewById(R.id.password)
 
         btnSignup.setOnClickListener {
-            val email = editEmail.text.toString()
+            val email = edtEmail.text.toString()
             val pass = edtPassword.text.toString()
             val username = edtUsername.text.toString()
 
@@ -53,9 +63,30 @@ class SigupActavity : AppCompatActivity() {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    //jump tp main activity
-                    val intent = Intent(this@SigupActavity, MainActivity::class.java)
-                    startActivity(intent)
+//                    Map<String,Objects> user = new HashMap<>()
+                    val user = hashMapOf(
+                        "username" to username,
+                        "email" to email,
+                        "password" to password
+                    )
+                    // Add a new document with a generated ID
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener { documentReference ->
+                            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                            //jump to main activity
+                            print("uso")
+                            val intent = Intent(this@SigupActavity, MainActivity::class.java)
+                            startActivity(intent)
+//                            finish()
+                        }
+                        .addOnFailureListener { e ->
+                            Toast.makeText(this@SigupActavity, "Error -" + e.message, Toast.LENGTH_SHORT).show()
+                        }
+//
+//                    //jump to main activity
+//                    val intent = Intent(this@SigupActavity, MainActivity::class.java)
+//                    startActivity(intent)
 
                 } else {
                     Toast.makeText(this@SigupActavity, "Error :(", Toast.LENGTH_SHORT).show()
